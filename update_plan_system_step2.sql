a5325d4b942e22b3c9a92164cc9c5e11dbf8c98a
dekpiano <dekpiano4815@gmail.com>
2025-11-14 11:05:50 +0700

    ลบไฟล์

--- update_plan_system_step2.sql
+++
@@ -1,8 +0,0 @@
--- This script updates the `tb_send_plan` table to populate the new `seplan_typeplan_id` column.
--- It matches the old text-based plan type (`seplan_typeplan`) with the corresponding ID
--- from the new `tb_send_plan_type` table.
-
-UPDATE `tb_send_plan`
-JOIN `tb_send_plan_type` ON `tb_send_plan`.`seplan_typeplan` = `tb_send_plan_type`.`type_name`
-SET `tb_send_plan`.`seplan_typeplan_id` = `tb_send_plan_type`.`type_id`
-WHERE `tb_send_plan`.`seplan_typeplan_id` IS NULL;
